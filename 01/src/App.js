0e58abd17486b5fc0c878bbabe4a818ebe86c689
Adrian Zawada <azawada93@gmail.com>
2022-04-27 00:39:49 +0200

    Complete lesson - basics of the React components

---
+++ 01/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+    <div className="wrapper">
+        <h1 className="mainHeader">Siema Elo</h1>
+        <h2 className="secondaryHeader">Adrian</h2>
+    </div>
+);
+
+export default App;
